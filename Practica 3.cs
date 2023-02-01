0f39e9e324a1fd7f1be85c2b9d6567ac50109d0a
LookGalee006 <111943692+LookGalee006@users.noreply.github.com>
2023-01-31 19:14:36 -0600

    Add files via upload

---
+++ Practica 3.cs
@@ -0,0 +1,75 @@
+﻿Console.WriteLine("Hello, World!");
+
+//break, continue, goto
+
+int n1,n2,n3;
+int par = 0, impar = 0;
+
+Console.WriteLine("Escribe el numero 1");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Escribe el numero 2");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Escribe el numero 3");
+n3 = Convert.ToInt32(Console.ReadLine());
+
+//Muestra del promedio //____________________________________I
+
+if ((n1 == n2) && (n2 == n3)) {
+    Console.WriteLine("El promedio es: {0}", n1);
+}
+
+//Muestra de los numeros pares_______________________________I
+
+if (n1 % 2 == 0)
+{
+    par = par + n1;
+}
+else { 
+    impar = impar + n1;
+}
+
+
+if (n2 % 2 == 0)
+{
+    par = par + n2;
+}
+else
+{
+    impar = impar + n2;
+}
+
+
+if (n3 % 2 == 0)
+{
+    par = par + n3;
+}
+else
+{
+    impar = impar + n3;
+}
+
+//Cual es el numero mas Grande_______________________________I
+
+if ((n1 > n2) && (n1 > n3)){
+    Console.WriteLine("El numero mayor es: {0}", n1);
+}
+
+if ((n2 > n1) && (n2 > n3))
+{
+    Console.WriteLine("El numero mayor es: {0}", n2);
+}
+
+if ((n3 > n2) && (n3 > n2))
+{
+    Console.WriteLine("El numero mayor es: {0}", n3);
+}
+
+Console.WriteLine("La suma de los numero pares es: {0}", par);
+Console.WriteLine("La suma de los numero impares es: {0}", impar);
+
+
+//hacer un programa que lea tres numeros, si los numeros son iguales se calculara el promedio, de lo contrario
+//se sumaran los numeros que son pares o impares entre ellos y por ultimo mostrara el mayor y menor de ellos//
+
---
+++ Practica 4.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.SymbolStore;
+
+Console.WriteLine("Hello, World!");
+//for
+/*
+for (int i = 0; i <= 10; i++) {
+    Console.WriteLine(i);
+}
+
+string text = "Este es un texto de ejemplo.";
+
+for (int i = 0; i < text.Length; i++)
+{
+    Console.WriteLine(text[i]);
+}
+
+Console.ReadLine();
+//while
+
+int w = 0;
+while (true)
+{
+    w++;
+    if (w > 10)
+    {
+        break;
+    }
+    else {
+        w++;
+    }
+}
+
+
+//do while
+*/
+
+
+//hacer un programa que lea un numero, si el numero no es par, se volvera a pedir, de lo contrario mostrara todos los elemenros que hay entre 0 y el//
+//nota el numero no debe ser negativio
+Boolean ban = true;
+do
+{
+    int n1 = 0;
+    Console.WriteLine("Escribe un numero");
+    n1 = Convert.ToInt32(Console.ReadLine());
+
+    if (n1 <= 0)
+    {
+        Console.WriteLine("El numero ingresado es menor a 0");
+    }
+    else
+    {
+        if (n1 % 2 == 0)
+        {
+            for (int i = 0; i <= n1; i++)
+            {
+                Console.WriteLine(i);
+            }
+            ban = false;
+        }
+        else {
+            Console.WriteLine("El numero no es par");
+        }
+    }
+
+
+} while(ban);
